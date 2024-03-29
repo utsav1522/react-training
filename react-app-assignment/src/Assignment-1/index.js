3132ade2396618c078917d478e3a0077fe4485d0
utsav1522 <utsav.jain@successive.tech>
2024-03-29 12:34:23 +0530

    Assignment-1

---
+++ react-app-assignment/src/Assignment-1/index.js
@@ -0,0 +1,7 @@
+import Greeting from "./Question-1";
+import UserCard from "./Question-2";
+import Weather from "./Question-3";
+import Counter from "./Question-4";
+import TaskList from "./Question-5/Question-5";
+import Button from "./Question-6";
+export { Greeting, UserCard, Weather, Counter, TaskList, Button };
