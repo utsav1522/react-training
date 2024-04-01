106eb5c2a5c6d275a06096ff1c87641684b468ad
utsav1522 <utsav.jain@successive.tech>
2024-04-01 21:39:47 +0530

    images

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  Counter,
-  PersonForm,
-  ToDoList,
-  RandomNumberGenerator,
-  CounterWithStep,
-  Clock,
-  Notification,
-} from "./Assignment-2";
+import Assignment2 from "./Assignment-2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Counter />
-    <hr />
-    <PersonForm />
-    <hr />
-    <ToDoList />
-    <hr />
-    <RandomNumberGenerator />
-    <hr />
-    <CounterWithStep />
-    <hr />
-    <Clock />
-    <hr />
-    <Notification />
-    <hr />
+    <Assignment2 />
   </>
 );
