145afd21cf4eedd4477f1234afac4b28f39156f5
utsav1522 <utsav.jain@successive.tech>
2024-04-03 15:52:31 +0530

    commit

---
+++ src/Assignment-4/Question-1/Question1.jsx
@@ -0,0 +1,16 @@
+/**
+ * Create a React component with an input field. 
+ * Implement it as a controlled component where the input value is controlled by the component's state. 
+ * When the user types into the input field, the component's state should update accordingly.
+ */
+
+import React, { useState } from "react";
+
+const Question1 = () => {
+  const [input, setInput] = useState("");
+  return (
+    <>
+    </>
+  );
+}
+export default Question1;
