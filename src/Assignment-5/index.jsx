d0402271b6dc7c6f011ce5b4f612f4ef42665437
utsav1522 <utsav.jain@successive.tech>
2024-04-14 11:35:23 +0530

    assignment - 5 router implementation

--- src/Assignment-5/index.jsx
+++ src/Assignment-5/index.jsx
@@ -1,37 +1,10 @@
-import FetchDataOne from "./components/Question-1";
-import Home from "./components/Question-12";
-import HomeThirteen from "./components/Question-13";
-import OriginalComponent from "./components/Question-15";
-import Component from "./components/Question-16";
-import FetchDataTwo from "./components/Question-2";
-import FetchDataThree from "./components/Question-3";
-import FetchDataFour from "./components/Question-4";
-import FetchDataFive from "./components/Question-5-6";
-import FetchDataSix from "./components/Question-8";
-import FetchDataSeven from "./components/Question-7";
-import IndxQuestionEight from "./components/Question-8";
-import IndexNine from "./components/Question-9";
-import IndexEleven from "./components/Question-11";
-import ProtectedComponent from "./components/Question-14";
-
+import AssignmentFiveRouter from "./AssignmentFiveRouter";
+import { BrowserRouter } from "react-router-dom";
 const Assignment5 = () => {
   return (
-    <>
-      {/* <FetchDataOne /> */}
-      {/* <FetchDataTwo /> */}
-      {/* <FetchDataThree /> */}
-      {/* <FetchDataFour /> */}
-      {/* <FetchDataFive /> */}
-      {/* <FetchDataSeven /> */}
-      {/* <IndxQuestionEight /> */}
-      {/* <IndexNine /> */}
-      {/* <IndexEleven /> */}
-      {/* <Home /> */}
-      {/* <HomeThirteen /> */}
-      <ProtectedComponent />
-      {/* <OriginalComponent /> */}
-      {/* <Component /> */}
-    </>
+    <BrowserRouter>
+      <AssignmentFiveRouter />
+    </BrowserRouter>
   );
 };
 export default Assignment5;
