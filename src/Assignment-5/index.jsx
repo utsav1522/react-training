b3e309d68a228f36401025c8690f5fa489a422e8
utsav1522 <utsav.jain@successive.tech>
2024-04-14 11:35:45 +0530

    assignment - 5 router implementation

--- src/Assignment-5/index.jsx
+++ src/Assignment-5/index.jsx
@@ -2,9 +2,9 @@ import AssignmentFiveRouter from "./AssignmentFiveRouter";
 import { BrowserRouter } from "react-router-dom";
 const Assignment5 = () => {
   return (
-    <BrowserRouter>
+    <>
       <AssignmentFiveRouter />
-    </BrowserRouter>
+    </>
   );
 };
 export default Assignment5;
